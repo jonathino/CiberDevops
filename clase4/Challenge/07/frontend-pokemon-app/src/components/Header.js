a57f5a66d9377a096d94e2ecc305b8ec020f7abb
Alex <alexballen53@gmail.com>
2023-05-26 21:19:10 -0500

    Se realiza solucion del ejercicio, se suban las pruebas imagenes, Dockerfile del back y del front y docker-compose

--- clase4/Challenge/07/frontend-pokemon-app/src/components/Header.js
+++ clase4/Challenge/07/frontend-pokemon-app/src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className="App-topbar">
-            <center>
-                <h1  style={{"fontFamily":"Roboto"}} >LISTA DE POKEMONES</h1>
-                <h3  style={{"fontFamily":"Roboto"}} >AQUÍ PODRÁS VER TUS POKEMONES</h3>
-            </center>
-        </div>
-    )
+  return (
+    <div className="App-topbar">
+      <center>
+        <h1 style={{ fontFamily: "Roboto" }}>LISTA DE POKEMONES</h1>
+        <h3 style={{ fontFamily: "Roboto" }}>AQUÍ PODRÁS VER TUS POKEMONES</h3>
+      </center>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
